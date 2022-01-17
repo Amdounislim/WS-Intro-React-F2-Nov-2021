d0e5f742b6184ddf82dbed345161052ddd0f9962
Amdounislim <amdouni.slim@gmail.com>
2022-01-17 18:03:05 +0100

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Footer.css'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Footer</h3>
+            </div>
+        )
+    }
+}
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Main() {
+    let name = "Main"
+    return (
+        <div>
+            <h2>this is {name}</h2>
+        </div>
+    )
+}
